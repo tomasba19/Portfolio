ce4cadaf45eef08e5038ac0e26e4c4a223ccce14
Tomas Barolo <tomasba19@gmail.com>
2023-09-19 19:30:00 -0300

    Home half done

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -5,6 +5,7 @@ import LogoTB from '../../assets/images/logofinal2.png'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faEnvelope, faHome, faUser } from '@fortawesome/free-solid-svg-icons'
 import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons'
+
 //<img className="sub-logo" src={LogoSubtitle} alt="subtitle" />
 
 const Sidebar = () => (
