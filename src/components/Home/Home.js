d35a4535bbf1b62b652ac7c91597d3f22639f935
Tomas Barolo <tomasba19@gmail.com>
2023-09-21 21:42:27 -0300

    Home fix

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -52,7 +52,7 @@ const Home = () => {
   useEffect(() => {
     const timerId = setTimeout(() => {
       setLetterClass('text-animate-hover')
-    }, 4000)
+    }, 6000)
 
     // Limpia el temporizador cuando el componente se desmonta
     return () => {
@@ -81,7 +81,7 @@ const Home = () => {
           <AnimatedLetters
             letterClass={letterClass}
             strArray={jobArray}
-            idx={20}
+            idx={29}
           />
         </h1>
         <Link to="/contact" className="flat-button">
