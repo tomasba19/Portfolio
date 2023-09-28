a688efcaf405cb859255327d9a65f418ae9f78d2
Tomas Barolo <tomasba19@gmail.com>
2023-09-27 21:16:13 -0300

    EmailJs

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,14 +1,97 @@
 import './Contact.scss'
-import Loader from '../Loader/Loader';
+import Loader from '../Loader/Loader'
+import AnimatedLetters from '../AnimatedLetters/AnimatedLetters.js'
+import { useEffect, useRef, useState } from 'react'
+import emailjs from '@emailjs/browser'
 
 const Contact = () => {
+  const [letterClass, setLetterClass] = useState('text-animate')
+  const refForm = useRef()
 
-    return (
-        <>
-        <div className="contact">'Hello'</div>
-        <Loader></Loader>
-        </>
-    )
+  useEffect(() => {
+    const timerId = setTimeout(() => {
+      setLetterClass('text-animate-hover')
+    }, 3000)
+
+    // Limpia el temporizador cuando el componente se desmonta
+    return () => {
+      clearTimeout(timerId)
+    }
+  }, [])
+
+  const sendEmail = (e) => {
+    e.preventDefault()
+
+
+    emailjs
+        .sendForm(
+            'service_92gqf3m',
+            'template_wls403w',
+            refForm.current,
+            'fFJ1y0kWP9zZwlMUy'
+        )
+        .then(
+            () => {
+                alert('Message successfully sent!')
+                window.location.reload(false)
+            },
+            () => {
+                alert('There was an error sending the message.')
+            }
+        )
+  }
+
+  return (
+    <>
+      <div className="container contact-page">
+        <div className="text-zone">
+          <h1>
+            <AnimatedLetters
+              letterClass={letterClass}
+              strArray={['C', 'o', 'n', 't', 'a', 'c', 't', ' ', 'm', 'e']}
+              idx={15}
+            />
+          </h1>
+          <div className="contact-form">
+            <form ref={refForm} onSubmit={sendEmail}>
+              <ul>
+                <li className="half">
+                  <input type="text" name="name" placeholder="Name" required />
+                </li>
+                <li className="half">
+                  <input
+                    type="email"
+                    name="email"
+                    placeholder="Email"
+                    required
+                  />
+                </li>
+                <li>
+                  <input
+                    type="text"
+                    name="subject"
+                    placeholder="Subject"
+                    required
+                  />
+                </li>
+                <li>
+                  <textarea
+                    placeholder="Message"
+                    name="message"
+                    required
+                  ></textarea>
+                </li>
+                <li>
+                  <input type="submit" className="flat-button" value="SEND" />
+                </li>
+              </ul>
+            </form>
+          </div>
+        </div>
+      </div>
+      <Loader></Loader>
+    </>
+  )
 }
 
-export default Contact;
+export default Contact
