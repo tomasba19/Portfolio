09ed638c325c646591901c043ed3eb1ea66f5206
Tomas Barolo <tomasba19@gmail.com>
2023-09-28 20:47:07 -0300

    GoogleMaps implemented

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -3,6 +3,7 @@ import Loader from '../Loader/Loader'
 import AnimatedLetters from '../AnimatedLetters/AnimatedLetters.js'
 import { useEffect, useRef, useState } from 'react'
 import emailjs from '@emailjs/browser'
+import { MapContainer, Marker, TileLayer } from 'react-leaflet'
 
 const Contact = () => {
   const [letterClass, setLetterClass] = useState('text-animate')
@@ -87,6 +88,19 @@ const Contact = () => {
               </ul>
             </form>
           </div>
+        </div>
+        <div className='info-map'>
+          Tomás Barolo
+          <br />
+          Río Cuarto, Argentina
+          <br />
+          <span>tomasba19@gmail.com</span>
+        </div>
+        <div className='map-wrap'>
+          <MapContainer center={[-33.12377252983355, -64.34903001135743]} zoom={13}>
+            <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+            <Marker position={[-33.12377252983355, -64.34903001135743]} />
+          </MapContainer>
         </div>
       </div>
       <Loader></Loader>
